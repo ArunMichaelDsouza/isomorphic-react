b0fb106b3b006cf87d21692b2b35812ec46c616b
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-09-24 03:23:02 +0530

    Base templating engine setup added

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import express from 'express';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import Button from './components/Button.jsx';
+import path from 'path';
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-	const ButtonEl = ReactDOM.renderToString(<Button />);
-	return res.send(ButtonEl);
+	const buttonEl = ReactDOM.renderToString(<Button />);
+	return res.render('index', { buttonEl });
 });
 
 app.listen(8080, () => {
